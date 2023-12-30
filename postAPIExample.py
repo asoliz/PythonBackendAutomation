d522355547a1a38404cf57e237f87215a8a9528c
asoliz <alansoliz@gmail.com>
2023-12-30 12:57:37 -0800

    Finished Section 4
    finishing sect 4

--- postAPIExample.py
+++ postAPIExample.py
@@ -1,18 +1,35 @@
 import requests
+import configparser
+from payLoad import *
+from utilities.configurations import *
+from utilities.resources import *
 
-from payLoad import addBookPayload
 
-addBook_response = requests.post('http://216.10.245.166/Library/Addbook.php', json=addBookPayload('genius'), headers={"Content-Type":"application/json"},)
+# requests.<typeOfRequest> is the method to send a request
+# requires url, body, header
+addBookUrl = getConfig()['API']['endpoint']+ApiResources.addBook
+header = {"Content-Type":"application/json"}
+addBook_response = requests.post(addBookUrl, json=addBookPayload('genius'), headers=header,)
 
+# view then store the response as json a variable
 print(addBook_response.json())
 response_json = addBook_response.json()
 
+# access to response is like accessing dictionary - key value pairs
+# accessing bookID in this example is by passing the key 'ID' and storing it for future use
 bookId = response_json['ID']
 
-deleteBook_response = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
-  "ID": bookId
-}, headers={"Content-Type":"application/json"},)
-res_json = deleteBook_response.json()
-print(res_json['msg'])
+# to delete, use the delete api and pass the stored variable 'bookId'
+deleteBookUrl = getConfig()['API']['endpoint']+ApiResources.deleteBook
+deleteBook_response = requests.post(deleteBookUrl, json={"ID": bookId}, headers=header,)
 
+# view then store the response
+print(deleteBook_response.json())
+res_json = deleteBook_response.json()
+
+# validate the response
 assert res_json['msg'] == "book is successfully deleted"
+
+authUrl = 'https://api.github.com/user'
+github_response = requests.get(authUrl, auth=('alansoliz@gmail.com', getPassword()))
+print(github_response.json())
